15a9f3da6498f44310919d1b13912dd4e98fac84
Vladislav Klyachin <git4vk@gmail.com>
2013-12-25 22:26:40 +0400

    Revert back, because we have different URLs for agent and user access

--- auth.go
+++ auth.go
@@ -13,32 +13,6 @@ type UserAuth struct {
 	ApiKey		string
 }
 
-// Combined set of fields to be able to distinguish Agent or User connections, e.g. for "agent log" connection
-type AgentOrUserAuth struct {
-	ApiKey		string
-	Uuid		string
-	Hostname	string
-	Username	string
-	Email		string
-	Provider	string
-}
-
-func (au *AgentOrUserAuth) AgentAuth() *AgentAuth {
-	return &AgentAuth{ApiKey: au.ApiKey, Uuid: au.Uuid, Hostname: au.Hostname, Username: au.Username}
-}
-
-func (au *AgentOrUserAuth) UserAuth() *UserAuth {
-	return &UserAuth{Email: au.Email, Provider: au.Provider, ApiKey: au.ApiKey}
-}
-
-func (au *AgentOrUserAuth) ContainsAgentCredentials() bool {
-	return au.ApiKey != "" && au.Uuid != ""
-}
-
-func (au *AgentOrUserAuth) ContainsUserCredentials() bool {
-	return au.Email != "" && au.Provider != "" && au.ApiKey != ""
-}
-
 type AuthResponse struct {
 	Error string
 }
