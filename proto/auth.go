0bf26be799da05ff67e63d0b3fde2c30018f0712
arvenil <arvenil@klecza.pl>
2014-01-31 15:55:12 +0100

    Revert for a moment "Remove *Auth.ApiKey" so tests on master could still work

--- proto/auth.go
+++ proto/auth.go
@@ -1,6 +1,7 @@
 package proto
 
 type AgentAuth struct {
+	ApiKey   string
 	Uuid     string
 	Hostname string
 	Username string
@@ -9,6 +10,7 @@ type AgentAuth struct {
 type UserAuth struct {
 	Email    string
 	Provider string
+	ApiKey   string
 }
 
 type AuthResponse struct {
