eb0bb84f2f6155fb27d3627f46f0e8e52d1c5c9a
arvenil <arvenil@klecza.pl>
2014-01-31 16:44:10 +0100

    Revert for a moment "Remove QanReport and MmReport" so tests on master could still work

--- proto/data.go
+++ proto/data.go
@@ -4,6 +4,26 @@ import (
 	"time"
 )
 
+type QanReport struct {
+	AgentUuid     string
+	StartTs       time.Time // UTC
+	EndTs         time.Time // UTC
+	SlowLogFile   string    // not slow_query_log_file if rotated
+	StartOffset   int64    // parsing starts
+	EndOffset     int64    // parsing stops, but...
+	StopOffset    int64    // ...parsing didn't complete if stop < end
+	RunTime       float64 // seconds
+	Global []byte
+	Class []byte
+}
+
+type MmReport struct {
+	AgentUuid string
+	Ts        time.Time // UTC
+	Duration  uint      // seconds
+	Metrics   []byte
+}
+
 type Transmission struct {
 	Created   time.Time // when Data was spool (UTC)
 	Hostname  string
