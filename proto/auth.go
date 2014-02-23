6af86d5ae211b0b3d61d030cbf7f5734bbb5a711
Daniel Nichter <daniel@percona.com>
2014-02-23 10:56:26 -0800

    Add back UserAgent: API needs it still (although it's not really used).

--- proto/auth.go
+++ proto/auth.go
@@ -7,6 +7,12 @@ type AgentAuth struct {
 	Username string
 }
 
+type UserAuth struct {
+	ApiKey   string
+	Email    string
+	Provider string
+}
+
 type AuthResponse struct {
 	Code  uint   // standard HTTP status (http://httpstatus.es/)
 	Error string // empty if auth ok (Code=200)
