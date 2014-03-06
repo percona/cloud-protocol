775f6f956cb4560ef6bc5dfa67e174796a86ea4e
Oleg Vakheta <oleg.vakheta@percona.com>
2014-03-06 15:08:14 +0200

    ahcnges in agent struct

--- proto/create.go
+++ proto/create.go
@@ -1,23 +1,7 @@
 package proto
 
-type VersionsData struct {
-	PerconaAgent string
-	MySQL        string
-}
-
-type Configuration struct {
-	Agent string
-	Log   string
-	Data  string
-}
-
 type AgentData struct {
-	Name string
-	Config Configuration
-}
-
-type CreateAgentData struct {
 	Hostname string
-	Versions VersionsData
-	Configs []AgentData
+	Versions map[string]string
+	Configs map[string]string
 }
