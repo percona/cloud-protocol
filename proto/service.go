decf5b6ae092a233a459e7a2e8427a247ae58054
Daniel Nichter <daniel@percona.com>
2014-04-22 17:04:23 -0700

    Add proto.UniqueInstance.

--- proto/service.go
+++ proto/service.go
@@ -6,6 +6,13 @@ var ExternalService map[string]uint = map[string]uint{
 	"mysql":  2,
 }
 
+// GET /instances returns a list of these, one for each unique hostname:
+type UniqueInstance struct {
+	Hostname string // Real or virtual server, Amazon RDS instance, etc.
+	ServerId uint   // ServerInstance.Id
+	MySQLId  uint   // MySQLInstance.Id
+}
+
 type ServiceInstance struct {
 	Service    string // one of ExternalService
 	InstanceId uint   // unique for Service: mysql-1, mysql-2, memcached-1, etc.c
@@ -21,6 +28,6 @@ type MySQLInstance struct {
 	Id       uint   // set by API
 	Hostname string // @@hostname[.port] if port != 3306
 	DSN      string // [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
-	Distro   string `json:",omitempty"`
+	Distro   string `json:",omitempty"` // MySQL Community Edition, Percona Server, etc.
 	Version  string `json:",omitempty"`
 }
