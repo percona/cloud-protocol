381a024a75ea96e0585b380be5906b85f3609eac
arvenil <arvenil@klecza.pl>
2014-10-06 17:45:55 +0200

    Update structs

--- proto/reports/reports.go
+++ proto/reports/reports.go
@@ -1,40 +1,39 @@
 package reports
 
-
 type EmailReport struct {
-	Meta Meta
+	Meta   Meta
 	Report Report
 }
 
 type Meta struct {
-	Id uint
+	Id       uint
 	TimeZone string
-	Begin string
-	End string
+	Begin    string
+	End      string
 }
 
 type Report struct {
-	Snapshot Snapshot
-	SlowQueries []Query
-	HighLoadQueries []Query
-	NewQueries []Query
+	Snapshot         Snapshot
+	SlowQueries      []Query
+	HighLoadQueries  []Query
+	FirstSeenQueries []Query
 }
 
 type Snapshot struct {
-	Queries int64
-	QueriesPct float64
-	QueryLoad float64
-	QueryLoadPct float64
-	ResponseTimePct95 float64
+	Queries              int64
+	QueriesPct           float64
+	QueryLoad            float64
+	QueryLoadPct         float64
+	ResponseTimePct95    float64
 	ResponseTimePct95Pct float64
 }
 
 type Query struct {
-	ClassId string
-	MetricSum float64
+	ClassId     string
+	FirstSeen   string `json:",omitempty"`
+	MetricSum   float64
 	MetricPct95 float64
-	MetricAvg float64
-	MetricMax float64
+	MetricAvg   float64
+	MetricMax   float64
 	MetricCount int64
 }
-
