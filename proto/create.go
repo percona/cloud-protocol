f1f634831fbd69e312c3c7e854bc1027b0ed3199
Carlos Salguero <cfsalguero@gmail.com>
2014-11-19 12:02:14 -0300

    PCT-1098

--- proto/create.go
+++ proto/create.go
@@ -13,11 +13,12 @@ type AgentConfig struct {
 }
 
 type Agent struct {
-	Uuid     string
-	Hostname string
-	Version  string
-	Running  bool              // connected to API?
-	QAN      bool              // has QAN config (running or not)?
-	Configs  []AgentConfig     `json:",omitempty"`
-	Links    map[string]string `json:",omitempty"`
+	Uuid       string
+	Hostname   string
+	Version    string
+	Running    bool              // connected to API?
+	QAN        bool              // has QAN config
+	QANRunning bool              // is QAN running or not?
+	Configs    []AgentConfig     `json:",omitempty"`
+	Links      map[string]string `json:",omitempty"`
 }
