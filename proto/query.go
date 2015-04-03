0731a019bcd86288856e87f9c4887d0455fc46ef
Daniel Nichter <daniel@percona.com>
2015-04-02 19:18:37 -0700

    PCT-657: Add structs: TableInfoResult, TableInfo, Table, ShowIndexRow, ShowTableStatus, TableInfoQuery.

--- proto/query.go
+++ proto/query.go
@@ -1,5 +1,9 @@
 package proto
 
+import (
+	"time"
+)
+
 type ExplainQuery struct {
 	ServiceInstance
 	Db    string
@@ -16,7 +20,6 @@ type ExplainRow struct {
 	SelectType   NullString
 	Table        NullString
 	Partitions   NullString // split by comma; since MySQL 5.1
-	CreateTable  NullString // @todo
 	Type         NullString
 	PossibleKeys NullString // split by comma
 	Key          NullString
@@ -25,3 +28,61 @@ type ExplainRow struct {
 	Rows         NullInt64
 	Extra        NullString // split by semicolon
 }
+
+type Table struct {
+	Db    string
+	Table string
+}
+
+type TableInfoQuery struct {
+	ServiceInstance
+	Create []Table // SHOW CREATE TABLE Db.Table
+	Index  []Table // SHOW INDEXES FROM Db.Table
+	Status []Table // SHOW TABLE STATUS FROM Db LIKE 'Table'
+}
+
+type ShowIndexRow struct {
+	Table        string
+	NonUnique    bool
+	KeyName      string
+	SeqInIndex   int
+	ColumnName   string
+	Collation    NullString
+	Cardinality  NullInt64
+	SubPart      NullInt64
+	Packed       NullString
+	Null         NullString
+	IndexType    string
+	Comment      NullString
+	IndexComment NullString
+}
+
+type ShowTableStatus struct {
+	Name          string
+	Engine        string
+	Version       string
+	RowFormat     string
+	Rows          NullInt64
+	AvgRowLength  NullInt64
+	DataLength    NullInt64
+	MaxDataLength NullInt64
+	IndexLength   NullInt64
+	DataFree      NullInt64
+	AutoIncrement NullInt64
+	CreateTime    time.Time
+	UpdateTime    time.Time
+	CheckTime     NullString // todo: need NullTime
+	Collation     NullString
+	Checksum      NullString
+	CreateOptions NullString
+	Comment       NullString
+}
+
+type TableInfo struct {
+	Create string          `json:",omitempty"`
+	Index  []ShowIndexRow  `json:",omitempty"`
+	Status ShowTableStatus `json:",omitempty"`
+	Errors []string        `json:",omitempty"`
+}
+
+type TableInfoResult map[string]*TableInfo
