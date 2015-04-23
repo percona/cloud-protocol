86a3eea9df9f776f2b68ad4e3b0ef7d079953bc8
Miguel Paolino <mpaolino@gmail.com>
2015-04-23 13:44:56 -0300

    Hostname instead of OSUUID in proto.Data, removal of deprecated AgentVersion

--- proto/v2/data.go
+++ proto/v2/data.go
@@ -7,12 +7,11 @@ import (
 type Data struct {
 	ProtocolVersion string    `json:",omitempty"` // protocol version in use
 	Created         time.Time // when Data was spooled (UTC)
-	OSUUID          string    // OS instance the agent is running on
+	Hostname        string    // OS instance name the agent is running on
 	Tool            string    // "qan" or "mm"
 	ContentType     string    // of Data ("application/json")
 	ContentEncoding string    // of Data ("gzip" or empty)
 	Data            []byte    // encoded qan.Report or mm.Report
-	AgentVersion    string    `json:",omitempty"` // version of the agent producing the data
 }
 
 type Response struct {
