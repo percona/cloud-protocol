cdc7c7d5e59a32a0ef6c7860752f0de21bebd714
Miguel Paolino <mpaolino@gmail.com>
2015-05-07 17:38:37 -0300

    Adds MMReport, InstanceStats and Stats to v2

--- proto/v2/data.go
+++ proto/v2/data.go
@@ -25,10 +25,10 @@ type Response struct {
 // QAN data struct, sent by the agent
 type QANReport struct {
 	UUID    string              // UUID of MySQL instance
-	StartTs time.Time           // of interval, UTC
-	EndTs   time.Time           // of interval, UTC
-	RunTime float64             // seconds parsing data
-	Global  *event.GlobalClass  // metrics for all data
+	StartTs time.Time           // Start time of interval, UTC
+	EndTs   time.Time           // Stop time of interval, UTC
+	RunTime float64             // Time parsing data, seconds
+	Global  *event.GlobalClass  // Metrics for all data
 	Class   []*event.QueryClass // per-class metrics
 	// slow log:
 	SlowLogFile     string `json:",omitempty"` // not slow_query_log_file if rotated
@@ -36,4 +36,28 @@ type QANReport struct {
 	StartOffset     int64  `json:",omitempty"` // parsing starts
 	EndOffset       int64  `json:",omitempty"` // parsing stops, but...
 	StopOffset      int64  `json:",omitempty"` // ...parsing didn't complete if stop < end
+}
+
+// MM data struct, sent by the agent
+type MMReport struct {
+	Ts       time.Time // start, UTC
+	Duration uint      // seconds
+	Stats    []*InstanceStats
+}
+
+// Stats for each metric from a service instance, computed at each report interval and sent by agent as part of MMReport.
+type InstanceStats struct {
+	UUID  string
+	Stats map[string]*Stats // keyed on metric name
+}
+
+// MM stats for interval, sent by agent as part or InstanceStats
+type Stats struct {
+	Cnt   int
+	Min   float64
+	Pct5  float64
+	Avg   float64
+	Med   float64
+	Pct95 float64
+	Max   float64
 }
