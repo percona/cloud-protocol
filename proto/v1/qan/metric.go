98f6f6906afbbc97c70641fd46410e33e1509ef4
Kamil Dziedzic <arvenil@klecza.pl>
2015-05-29 16:32:16 +0200

    Make query_count also nullable for cases without data

--- proto/v1/qan/metric.go
+++ proto/v1/qan/metric.go
@@ -3,17 +3,17 @@ package qan
 import protoV1 "github.com/percona/cloud-protocol/proto/v1"
 
 type MetricsSummary struct {
-    Metrics    []Metric
-    QueryCount int64
+	Metrics    []Metric
+	QueryCount protoV1.NullInt64
 }
 
 type Metric struct {
-	Name        string
-	Total       protoV1.NullFloat32
-	Avg         protoV1.NullFloat32
-	Max         protoV1.NullFloat32
-	Min         protoV1.NullFloat32
-	Pct_95      protoV1.NullFloat32
-	Stddev      protoV1.NullFloat32
-	Median      protoV1.NullFloat32
+	Name   string
+	Total  protoV1.NullFloat32
+	Avg    protoV1.NullFloat32
+	Max    protoV1.NullFloat32
+	Min    protoV1.NullFloat32
+	Pct_95 protoV1.NullFloat32
+	Stddev protoV1.NullFloat32
+	Median protoV1.NullFloat32
 }
