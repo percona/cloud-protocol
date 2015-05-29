d472ee8d9ca93fde42acc5bb0fb224a741859556
Kamil Dziedzic <arvenil@klecza.pl>
2015-05-29 15:21:34 +0200

    NullFloat32 for those FLOAT values in db

--- proto/v1/null.go
+++ proto/v1/null.go
@@ -2,7 +2,7 @@ package proto
 
 import (
 	"bytes"
-	"database/sql"
+    "database/sql"
 	"encoding/json"
 )
 
@@ -88,3 +88,35 @@ func (n *NullFloat64) UnmarshalJSON(b []byte) error {
 	n.Valid = true
 	return nil
 }
+
+// sql package doesn't allow me to create proper float32
+// so conversion is used to achieve the same
+type NullFloat32 struct {
+    sql.NullFloat64
+}
+
+func (n *NullFloat32) MarshalJSON() (b []byte, err error) {
+    if !n.Valid {
+        return []byte("null"), nil
+    }
+    return json.Marshal(float32(n.Float64))
+}
+
+func (n *NullFloat32) UnmarshalJSON(b []byte) error {
+    if bytes.Equal(b, []byte("null")) {
+        n.Float64 = 0
+        n.Valid = false
+        return nil
+    }
+
+    var v float32
+    err := json.Unmarshal(b, &v)
+    if err != nil {
+        return err
+    }
+
+    n.Valid = true
+    n.Float64 = float64(v)
+    return nil
+}
+
